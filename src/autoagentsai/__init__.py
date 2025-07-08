1c893f31e0d7c45f08b9c610546b2540887d5a0c
Huazaihuazaihuazai <2249767626@qq.com>
2025-07-08 16:37:55 +0800

    新增知识库创建和查询接口封装

--- src/autoagentsai/__init__.py
+++ src/autoagentsai/__init__.py
@@ -1,9 +1,16 @@
-from .models import ChatRequest, ImageInput, ChatHistoryRequest, FileInput
-from .client.ChatClient import ChatClient
-from .uploader import FileUploader, create_file_like
 from . import api
+from . import kb_api
+from .client.ChatClient import ChatClient
+from .client.KbClient import KbClient
+from .kb_models import KbCreateRequest, KbQueryRequest, KbModifyRequest
+from .models import ChatRequest, ImageInput, ChatHistoryRequest, FileInput
+from .uploader import FileUploader, create_file_like
 
-__all__ = ["ChatRequest", "ImageInput", "ChatClient", "FileUploader", "ChatHistoryRequest", "FileInput", "api", "create_file_like"]
+__all__ = [
+    "ChatRequest", "ImageInput", "ChatClient", "KbClient", "FileUploader",
+    "ChatHistoryRequest", "FileInput", "api", "kb_api", "create_file_like",
+    "KbCreateRequest", "KbQueryRequest"
+]
 
 
 def main() -> None:
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from autoagentsai.client import ChatClient
 
 client = ChatClient(
     agent_id="90b60436c09b43e5b6d05a31abf8c662",
-    personal_auth_key="e7a964a7e754413a9ea4bc1395a38d39",
-    personal_auth_secret="r4wBtqVD1qjItzQapJudKQPFozHAS9eb"
+    auth_key="e7a964a7e754413a9ea4bc1395a38d39",
+    auth_secret="r4wBtqVD1qjItzQapJudKQPFozHAS9eb"
 )
 
 i = 1
